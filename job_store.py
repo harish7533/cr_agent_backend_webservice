223f74899c6522fcf511ed347aac0fc7e0bc7856
harish7533 <harishbalamurugan.2002@gmail.com>
2026-07-22 16:00:14 +0530

    Added files via upload
    Added these files via upload to provide as a web service to Render.

---
+++ job_store.py
@@ -0,0 +1,19 @@
+import uuid
+
+jobs = {}
+
+def create_job():
+    job_id = str(uuid.uuid4())
+    jobs[job_id] = {
+        "status": "started",
+        "progress": 0,
+        "message": "Job initiated"
+    }
+    return job_id
+
+def update_job(job_id, data):
+    if job_id in jobs:
+        jobs[job_id].update(data)
+
+def get_job(job_id):
+    return jobs.get(job_id, {"error": "Job not found"})
